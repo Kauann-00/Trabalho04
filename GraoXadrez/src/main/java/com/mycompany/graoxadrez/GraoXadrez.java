29e1c791ecd14bcec09ac98f6773a46c36787c6b
Kauann-00 <marcoskauan2007@gmail.com>
2025-05-13 01:38:34 -0300

    Update GraoXadrez.java

--- GraoXadrez/src/main/java/com/mycompany/graoxadrez/GraoXadrez.java
+++ GraoXadrez/src/main/java/com/mycompany/graoxadrez/GraoXadrez.java
@@ -1,4 +1,4 @@
-// 
+//Estrutura de repetição
 
 package com.mycompany.graoxadrez;
 
@@ -6,16 +6,13 @@ import java.math.BigInteger;
 
 public class GraoXadrez {
     public static void main(String[] args) {
-        // Total de casas no tabuleiro
         int casas = 64;
         
-        // Calcula 2^64 - 1 usando BigInteger
         BigInteger totalGraos = BigInteger.ZERO;
         for (int i = 0; i < casas; i++) {
             totalGraos = totalGraos.add(BigInteger.TWO.pow(i));
         }
         
-        // Exibe o resultado
         System.out.println("O total de grãos que o monge recebeu é: " + totalGraos);
     }
 }
